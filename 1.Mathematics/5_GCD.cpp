f6913e204f636bad5b5aa76ac631f74bde19aec1
Shobhit Gupta <shobhitgpt01@gmail.com>
2024-01-10 02:14:33 +0530

    5

---
+++ 1.Mathematics/5_GCD.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    GCD of two numbers is the greatest number that divides both A , B.
+    We can find GCD of two numbers using two approach
+    1. Euclidean Approach (fastest)
+        based on GCD(A,B) = GCD(B,A-B) or GCD(B,R) where R is the remainder A%B
+        and GCD(A,0) = A
+    2. Prime  factorisation
+
+*/
+int euclid_GCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return euclid_GCD(b, a % b);
+}
+void prime_factors(int num, vector<int> &v)
+{
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            v.push_back(i);
+            num = num / i;
+        }
+    }
+
+    if (num > 1)
+        v.push_back(num);
+}
+
+int prime_factors_GCD(int a, int b)
+{
+    vector<int> v1;
+    vector<int> v2;
+
+    prime_factors(a, v1);
+    prime_factors(b, v2);
+
+    vector<int> common_pf;
+    for (int factor : v1)
+    {
+        if (find(v2.begin(), v2.end(), factor) != v2.end())
+        {
+            common_pf.push_back(factor);
+            v2.erase(find(v2.begin(), v2.end(), factor));
+        }
+    }
+
+    int ans = 1;
+    for (int factor : common_pf)
+    {
+        ans *= factor;
+    }
+    return ans;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    // cout << euclid_GCD(a, b);
+    cout << prime_factors_GCD(a, b);
+}
